cd884146063d991de2ef99dd889502802bf5fcb8
bbgeneralplus2028-star <bbgeneralplus2028@gmail.com>
2026-05-08 20:34:37 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [lawsuits, setLawsuits] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/lawsuits")
+      .then(res => res.json())
+      .then(data => setLawsuits(data));
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>ClassAction AI Radar Pro</h1>
+
+      {lawsuits.map(l => (
+        <div key={l.id} style={{ border: "1px solid #ccc", margin: 10, padding: 10 }}>
+          <h3>{l.title}</h3>
+          <p>{l.summary}</p>
+          <a href={l.url} target="_blank">View Case</a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
